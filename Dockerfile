d62f17576148fb4f1b1f0754e9d2205409a93d2b
Robbie Leslie <rwl228@cornell.edu>
2026-04-18 18:44:22 -0400

    Added microros to container

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,19 @@
 FROM docker.io/osrf/ros:humble-desktop-jammy
 
+COPY --from=docker.io/microros/micro-ros-agent:humble /uros_ws /uros_ws
+
 RUN apt-get update && apt-get install -y python3-pip && rm -rf /var/lib/apt/lists/*
 
 RUN pip3 install pyzmq
 
+# Build custom msgs into the image
+RUN mkdir -p /msgs_ws/src && cd /msgs_ws/src && \
+    git clone https://github.com/leslier7/lost_book_msgs.git && \
+    cd /msgs_ws && \
+    . /opt/ros/humble/setup.sh && \
+    colcon build && \
+    rm -rf build log
+
 RUN echo "source /opt/ros/humble/setup.bash" >> ~/.bashrc
+RUN echo "source /uros_ws/install/setup.bash" >> ~/.bashrc
+RUN echo "source /msgs_ws/install/setup.bash" >> ~/.bashrc
--- setup.sh
+++ setup.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 
+#DEVICE_ARGS=""
+#for DEV in /dev/ttyUSB0 /dev/ttyUSB1 /dev/ttyACM0 /dev/ttyACM1; do
+#  if [ -e "$DEV" ]; then
+#    DEVICE_ARGS="--device=$DEV:$DEV"
+#    echo "Found device: $DEV"
+#    break
+#  fi
+#done
+
 if docker ps -q -f name=my_ros2_container | grep -q .; then
   docker exec -it -w /root/ros2_ws my_ros2_container bash
 else
   docker rm -f my_ros2_container 2>/dev/null
   docker run -it \
-    --name my_ros2_container \
-    --network=host \
-    -v ~/ros2_ws:/root/ros2_ws \
-    my_ros2 \
-    bash -c "cd /root/ros2_ws && exec bash"
+  --name my_ros2_container \
+  --network=host \
+  --privileged \
+  -v /home/$SUDO_USER/ros2_ws:/root/ros2_ws \
+  my_ros2 \
+  bash -c "cd /root/ros2_ws && exec bash"
 fi
