28d4e726a056a91480c42f3b9ea775871a522eab
Robbie Leslie <robbie.w.leslie@gmail.com>
2026-04-25 15:41:46 -0400

    Added commnts to mock code

--- cv_mock.py
+++ cv_mock.py
@@ -22,10 +22,12 @@ push.bind("tcp://*:5556")
 print("CV mock ready — waiting for bridge...")
 
 while True:
-    msg = pull.recv_string()
+    msg = pull.recv_string() # This is when robot sends to take photo
     print(f"Received: '{msg}'")
 
+    # Code to take and process photo should go here
     input("Press Enter to simulate photo taken and processed...")
 
+    #This is CV saying that it has taken the photo, robot can move
     push.send_string("ready")
     print("Sent: 'ready'\n")
